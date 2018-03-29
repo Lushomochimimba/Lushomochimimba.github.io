cef4e2376f72276865a8ef5abb26c6f7ec78d589
Lushomo chimimba <lushomochimimba9@gmail.com>
2018-03-29 17:48:47 +0200

    updated landing page

---
+++ js/script.js
@@ -0,0 +1,40 @@
+var typed = new Typed('.element', {
+strings: ["front developer", "a CSS specialist", "a soon to be fully fledged web developer", "hungry", "going to get snacks now", "gonna go eat for a bit", "going to be back soon, maybe", "gonna take a break and put all this on loop"],
+typeSpeed: 60,
+backSpeed: 30,
+backDelay: 2000,
+loop: true
+});
+// scroll to projects
+  $('#bt').click( function() {
+    $('html, body').animate({
+         scrollTop: $('#D2').offset().top
+    }, 1000);
+});// scroll to contact me form
+$('#bt1').click( function() {
+    $('html, body').animate({
+         scrollTop: $('#D3').offset().top
+    }, 1000);
+});// scroll to footer
+$('#bt2').click( function() {
+    $('html, body').animate({
+         scrollTop: $('#D4').offset().top
+    }, 1000);
+
+});
+$('#bt3').click( function() {
+    $('html, body').animate({
+         scrollTop: $('#D5').offset().top
+    }, 1000);
+
+});
+$('#home').click( function() {
+    $('html, body').animate({
+         scrollTop: $('#D1').offset().top
+    }, 1000);
+});// scroll to the top of the page
+if ($('.scrollToTop')[0]){
+    $('.scrollToTop').click(function(){
+         $('html,body').animate({ scrollTop: 0 }, 100);
+    });
+}
