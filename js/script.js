825590a0ef19aeddf7ff04552de5858e3fa51e56
lushomo chimimba <lushomochimimba9@gmail.com>
2018-05-12 23:34:59 +0200

    added two projects in my work section and updated text in head section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.element', {
-strings: ["front developer", "a CSS specialist", "a soon to be fully fledged web developer", "hungry", "going to get snacks now", "gonna go eat for a bit", "going to be back soon, maybe", "gonna take a break and put all this on loop"],
+strings: ["am a Front and Back end developer", "a CSS specialist", "a soon to be full fledged web developer", "I hope you enjoy your stay on my site"],
 typeSpeed: 60,
 backSpeed: 30,
 backDelay: 2000,
